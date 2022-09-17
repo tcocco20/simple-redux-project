82d246476e0e7cc247fa784f254bb8d8c91117aa
tcocco20 <theococco@yahoo.com>
2022-09-16 19:47:21 -0700

    added store and actions and connected store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import store from "./store";
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
