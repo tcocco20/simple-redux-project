a4408dba4739265c43be3df2af9b9199b2350645
tcocco20 <theococco@yahoo.com>
2022-09-21 16:09:18 -0700

    split state slices and reducers to separate files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Header.module.css";
 
-import { authActions } from "../store";
+import { authActions } from "../store/auth-slice";
 
 const Header = () => {
   const authenticated = useSelector(state => state.auth.isAuthenticated);
